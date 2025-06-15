d032413c2805cdd4cb80fa0d219475ee9d5ecc1d
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-06-15 02:57:21 -0300

    feat/class 12: styled navbar

--- classes/R_12/pageNoticias/src/components/header/Header.jsx
+++ classes/R_12/pageNoticias/src/components/header/Header.jsx
@@ -1,11 +1,11 @@
 function Header(){
     return(
-        <header className="w-full flex bg-amber-400 justify-between">
-            <p>Portal<b>NOTICIAS</b></p>
-            <ul>
-                <li>Noticias</li>
-                <li>Contacto</li>
-                <li>Sobre nosotros para que aprendas</li>
+        <header className="w-full flex bg-[#6f75e2] justify-between px-3 items-center min-h-14">
+            <p className="text-3xl font-bold text-white cursor-pointer">Portal<b>NOTICIAS</b></p>
+            <ul className="flex gap-8 justify-between">
+                <li><a href="" className="p-1.5 rounded text-white hover:bg-[#5b5fae]">Noticias</a></li>
+                <li><a href="" className="p-1.5 rounded text-white hover:bg-[#5b5fae]">Contacto</a></li>
+                <li><a href="" className="p-1.5 rounded text-white hover:bg-[#5b5fae]">Sobre nosotros</a></li>
             </ul>
         </header>
     )
