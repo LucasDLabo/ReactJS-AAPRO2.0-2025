31c406cd17181de0ae7d25ca827ddd096be2afe3
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-06-15 01:44:06 -0300

    feat/class 12: added head navbar tag + fix #root selector making trouble with body padding

--- classes/R_12/pageNoticias/src/App.jsx
+++ classes/R_12/pageNoticias/src/App.jsx
@@ -1,24 +1,28 @@
 import NewsCard from './components/cards/NewsCard/NewsCard.jsx';
 import CardContainer from './components/cards/CardContainer/CardContainer.jsx';
+import Header from './components/header/Header.jsx';
 import './App.css';
 import newsData from './components/cards/NewsCard/NewsData.js'
 
 function App() {
   return (
     <>
-      <CardContainer>
-        {newsData.map( (news)=>(
-          <NewsCard 
-            key={news.id} 
-            title={news.title} 
-            description={news.description} 
-            source={news.source} 
-            imgURL={news.imgURL} 
-            imgALT={news.imgALT} 
-            date={news.date}
-          />
-        ) )}
-      </CardContainer>
+      <Header />
+      <main>
+        <CardContainer>
+          {newsData.map( (news)=>(
+            <NewsCard 
+              key={news.id} 
+              title={news.title} 
+              description={news.description} 
+              source={news.source} 
+              imgURL={news.imgURL} 
+              imgALT={news.imgALT} 
+              date={news.date}
+            />
+          ) )}
+        </CardContainer>
+      </main>
     </>
   )
 }
---
+++ classes/R_12/pageNoticias/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+function Header(){
+    return(
+        <header className="flex bg-amber-400">
+            <p>Portal Noticias</p>
+            <ul>
+                <li>Noticios</li>
+                <li>Contacto</li>
+                <li>Sobre nosotros</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
