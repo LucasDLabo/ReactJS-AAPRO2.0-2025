2b5b5db9b318703912cd4938bd36a02a0cb57ffd
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-06-15 01:35:00 -0300

    fix/class 12: minor fix with img placeholder

--- classes/R_12/pageNoticias/src/components/cards/NewsCard/NewsCard.jsx
+++ classes/R_12/pageNoticias/src/components/cards/NewsCard/NewsCard.jsx
@@ -4,13 +4,13 @@ function NewsCard( { title, description, source, imgURL, imgALT, date} ){
     
     {
         if (imgURL == "") {
-            imgURL = "https://imgs.search.brave.com/LhkMCDZM_xXAlzZYt22XIs04m3MhW8aRKZl3bZ3eLWI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA2Lzg2LzE5LzM0/LzM2MF9GXzY4NjE5/MzQwN19ESFp3amV5/ZEJPUjF0RURrTEF6/d00zdzVrWXN0Unp6/Qi5qcGc"
+            imgURL = "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
         }
     }
 
     return (
         <section className="mb-5 flex min-h-[400px] max-w-sm flex-col items-center rounded-lg border border-gray-400 bg-gray-200 shadow-lg">
-            <img src={imgURL} alt={imgALT} className="aspect-video w-full rounded-t-lg object-contain" />
+            <img src={imgURL} alt={imgALT} className="aspect-video w-full rounded-t-lg " />
             <div className="p-3 h-full flex flex-col text-gray-700">
                 <h2 className="font-bold mb-1.5 text-xl">{title}</h2>
                 <p className="text-justify mb-1.5">{description}</p>
