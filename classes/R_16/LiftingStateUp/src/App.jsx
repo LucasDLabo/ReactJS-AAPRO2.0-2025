5df1efbc568dc9c3fe75db0a6413f8ae55c98dbd
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-07-08 19:29:50 -0300

    feat: send form

--- classes/R_16/LiftingStateUp/src/App.jsx
+++ classes/R_16/LiftingStateUp/src/App.jsx
@@ -3,16 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Adding from './components/Adding'
 import Subtract from './components/Subtract'
+import Encuesta from './components/Entregable/Encuesta'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(10);
+
+  const [respuesta, setRespuesta] = useState("");
 
   return (
     <>
       <h1>Contador: {count}</h1>
       <Adding count={count} onChangeAdd={setCount} />
       <Subtract count={count} onChangeSub={setCount} />
+      <Encuesta></Encuesta>
     </>
   )
 }
