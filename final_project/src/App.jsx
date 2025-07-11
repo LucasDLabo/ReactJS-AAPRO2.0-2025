50078af5dd5499d535f8212037d8cdc09db659bd
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-07-11 20:21:23 -0300

    feat/finalProject: appointments now are saved in localStorage + fix: App.jsx indentation

--- final_project/src/App.jsx
+++ final_project/src/App.jsx
@@ -7,43 +7,46 @@ import specialties from './utils/specialties.js'
 import timetables from './utils/timetables.js'
 
 function App() {
-  const [appoint, setAppoint] = useState(exampleData);
+    const [appoint, setAppoint] = useState( () => {
+        const localstorage = localStorage.getItem('appointments');
+        return localstorage ? JSON.parse(localstorage) : localStorage.setItem('appointments', JSON.stringify(exampleData));
+    });
 
-  function deleteAppoint(id){
-    console.log(`id pasado ${id} - Turno borrado`)
-    setAppoint(appoint.filter((i) => i.id !== id));
-  }
+    function deleteAppoint(id){
+        console.log(`id pasado ${id} - Turno borrado`)
+        const updatedAppoint = appoint.filter((i) => i.id !== id);
+        setAppoint(updatedAppoint);
+        localStorage.setItem('appointments', JSON.stringify(updatedAppoint));
+    }
 
-  function createAppoint( {name, date, time, specialty} ){
-    const newAppoint = {
-      id: Date.now(), name, date, time, specialty
-    };
-    setAppoint([newAppoint, ...appoint]);
+    function createAppoint( {name, date, time, specialty} ){
+        const newAppoint = {
+        id: Date.now(), name, date, time, specialty
+        };
+        setAppoint([newAppoint, ...appoint]);
+        localStorage.setItem('appointments', JSON.stringify([newAppoint, ...appoint]));
+    }
 
-  }
+    return (
+        <>  
+            <AppointmentForm onCreate={createAppoint} specialties={specialties} timetables={timetables} appoint={appoint}></AppointmentForm>
 
-  return (
-    <>  
-      <AppointmentForm onCreate={createAppoint} specialties={specialties} timetables={timetables} appoint={appoint}></AppointmentForm>
+            {
+                appoint.map( (data)=> (
+                <AppointmentList 
+                    key={data.id}
+                    id={data.id}
+                    name={data.name}
+                    date={data.date}
+                    time={data.time}
+                    specialty={data.specialty}
 
-      {
-        appoint.map( (data)=> (
-          
-          <AppointmentList 
-            key={data.id}
-            id={data.id}
-            name={data.name}
-            date={data.date}
-            time={data.time}
-            specialty={data.specialty}
-
-            onDelete={deleteAppoint}
-          />
-
-        ) )
-      }
-    </>
-  )
+                    onDelete={deleteAppoint}
+                />
+                ) )
+            }
+        </>
+    )
 }
 
 export default App
