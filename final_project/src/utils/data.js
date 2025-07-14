272fd22a1e0444784e8ebba85aae02bdc12d65ae
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-07-14 18:43:13 -0300

    update/finalProject: added more appointments in the mockup data

--- final_project/src/utils/data.js
+++ final_project/src/utils/data.js
@@ -1,5 +1,33 @@
 export default[
     {
+        id: 7,
+        name: 'Estefani Gomez',
+        date: '2025-07-10',
+        time: '10:00',
+        specialty: 'Medical Consultation',
+    },
+    {
+        id: 6,
+        name: 'Sofia Lazio',
+        date: '2025-07-06',
+        time: '8:00',
+        specialty: 'Cardiology',
+    },
+    {
+        id: 5,
+        name: 'Esteban Guemes',
+        date: '2025-07-02',
+        time: '9:40',
+        specialty: 'Dentistry',
+    },
+    {
+        id: 4,
+        name: 'Cesar Arturo',
+        date: '2025-07-04',
+        time: '9:00',
+        specialty: 'Medical Consultation',
+    },
+    {
         id: 3,
         name: 'Matias Rojas',
         date: '2025-07-04',
