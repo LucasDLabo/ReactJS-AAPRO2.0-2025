0524435d1c656bf012177701e4f56d99cb3ecb52
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-07-16 17:17:27 -0300

    feat/finalProject: dark style for navbar

--- final_project/src/components/header/Navbar.jsx
+++ final_project/src/components/header/Navbar.jsx
@@ -38,19 +38,19 @@ function Navbar(){
 
     return(
         <>
-            <nav className="flex justify-between h-12 bg-gray-50">
-                <h3 className=" px-10 text-2xl italic font-bold items-center flex justify-center">Medical Institution</h3>
-                <ul className="flex gap-5 px-5 items-center font-bold text-gray-500 ">
-                    <li className="hover:text-black"><a href="">HOME</a></li>
-                    <li className="text-blue-800"><a href="">APPOINTMENTS</a></li>
-                    <li className="hover:text-black"><a href="">SERVICES</a></li>
-                    <li className="hover:text-black"><button onClick={toggleSettingsWindow} className='cursor-pointer'>Settings</button></li>
+            <nav className="flex justify-between h-12 bg-gray-50 dark:bg-gray-900">
+                <h3 className=" px-10 text-2xl italic font-bold items-center flex justify-center dark:text-gray-200">Medical Institution</h3>
+                <ul className="flex gap-5 px-5 items-center font-bold text-gray-500 dark:text-gray-200 ">
+                    <li className="hover:text-black dark:hover:text-blue-700"><a href="">Home</a></li>
+                    <li className="hover:text-black dark:hover:text-blue-700"><a href="">Services</a></li>
+                    <li className="text-blue-700"><a href="">APPOINTMENTS</a></li>
+                    <li className="hover:text-black dark:hover:text-blue-700"><button onClick={toggleSettingsWindow} className='cursor-pointer'>Settings</button></li>
                 </ul>
                 {isSettingsOpen && (
-                    <div className='absolute bg-white shadow-black shadow-2xl p-3 border-2 border-gray-300 top-12 right-0 z-50'>
+                    <div className='absolute bg-white dark:bg-gray-800 dark:border-gray-500 shadow-black shadow-2xl p-3 border-2 border-gray-300 top-12 right-0 z-50'>
                         <ul>
                             <li><button className='btn-settings' title={`Switch to ${theme === "dark" ? "light theme" : "dark theme"} `} onClick={toggleTheme}>Change Theme</button></li>
-                            <hr className='text-gray-200' />
+                            <hr className='text-gray-200 dark:text-gray-500' />
                             <li><button className='btn-settings'>Logout</button></li>
                         </ul>
                     </div>
