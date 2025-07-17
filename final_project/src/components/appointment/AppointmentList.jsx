1d86d569c4be96ef9eb94ebc26b3bead3eacac6b
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-07-17 15:49:02 -0300

    fix/finalProject: typo in the name of parseLocaldate

--- final_project/src/components/appointment/AppointmentList.jsx
+++ final_project/src/components/appointment/AppointmentList.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import Modal from '../../modal/Modal'
+import { parseLocalDate } from '../../utils/parseLocaldate'
+
 
 function AppointmentList( {id, name, date, formatted_date, time, specialty, onDelete, timetables, specialties, onUpdate, appoint} ){
 
