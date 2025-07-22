2beb4446dbba6debd528afae88848e8b1eec367e
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-07-22 20:02:07 -0300

    refactor/mobile: improved tailwind classes sorting

--- final_project/src/components/header/Navbar.jsx
+++ final_project/src/components/header/Navbar.jsx
@@ -41,19 +41,19 @@ function Navbar(){
 
     return(
         <>
-        {/* Menú Mobile Full Screen */}
+        {/* When mobile menu is open... */}
             {isMobileMenuOpen && (
                 <section>
-                    <div className="fixed inset-0 z-10 bg-black/60" onClick={toggleMobileMenuWindow}/>
+                    <div className="fixed z-10 h-screen w-full bg-black/60" onClick={toggleMobileMenuWindow}/>
                 
-                    <div className="fixed z-10 top-0 right-0 w-7/12 h-screen bg-gray-50 dark:bg-gray-900 dark:text-gray-200 flex flex-col items-center justify-center text-3xl transition-all duration-300">
-                        <ul className="flex flex-col gap-2 px-5 font-bold text-gray-500 dark:text-gray-200 text-2xl">
+                    <div className="fixed top-0 right-0 z-10 flex h-screen w-7/12 flex-col items-center justify-center bg-gray-50 text-3xl dark:bg-gray-900 dark:text-gray-200">
+                        <ul className="flex flex-col gap-2 px-5 text-2xl font-bold text-gray-500 dark:text-gray-200">
                             <li><a href="">Home</a></li>
                             <li><a href="">Services</a></li>
                             <li className="text-blue-700"><button title='Current Page'>Appointments</button></li>
                             <hr />
                             <li>
-                                <button className="rounded text-start font-medium dark:font-medium text-gray-400 text-xl gap-1 dark:text-gray-600 flex items-center" title={`Switch to ${theme === 'dark' ? 'light theme' : 'dark theme'}`} onClick={toggleTheme}>
+                                <button className="flex items-center gap-1 rounded text-start text-xl font-medium text-gray-400 dark:font-medium dark:text-gray-600" title={`Switch to ${theme === 'dark' ? 'light theme' : 'dark theme'}`} onClick={toggleTheme}>
                                     Change Theme
                                     {
                                         theme === 'dark' ? 
@@ -69,7 +69,7 @@ function Navbar(){
                                 </button>
                             </li>
                             <li>
-                                <button className="rounded text-start font-medium dark:font-medium text-gray-400 text-xl gap-1 dark:text-gray-600 flex items-center" title='Exit'>
+                                <button className="flex items-center gap-1 rounded text-start text-xl font-medium text-gray-400 dark:font-medium dark:text-gray-600" title='Exit'>
                                     Logout
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
                                         <path fillRule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v9a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM6.166 5.106a.75.75 0 0 1 0 1.06 8.25 8.25 0 1 0 11.668 0 .75.75 0 1 1 1.06-1.06c3.808 3.807 3.808 9.98 0 13.788-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
@@ -81,45 +81,46 @@ function Navbar(){
                 </section>
             )}
 
-            <nav className="flex h-12 justify-between bg-gray-50 dark:bg-gray-900 z-20 relative">
-                <h3 className="flex items-center justify-center px-2 md:px-10 py-3 text-xl font-bold text-blue-800 italic dark:text-gray-200">
+            {/* Navbar */}
+            <nav className="relative z-20 flex h-12 justify-between bg-gray-50 dark:bg-gray-900">
+                <h3 className="flex items-center justify-center px-2 py-3 text-xl font-bold text-blue-800 italic md:px-10 dark:text-gray-200">
                     <svg xmlns="http://www.w3.org/2000/svg" className='size-6 mt-1' fill="currentColor" viewBox="0 0 256 256">
                         <path d="M240,128a8,8,0,0,1-8,8H204.94l-37.78,75.58A8,8,0,0,1,160,216h-.4a8,8,0,0,1-7.08-5.14L95.35,60.76,63.28,131.31A8,8,0,0,1,56,136H24a8,8,0,0,1,0-16H50.85L88.72,36.69a8,8,0,0,1,14.76.46l57.51,151,31.85-63.71A8,8,0,0,1,200,120h32A8,8,0,0,1,240,128Z"></path>
                     </svg>
                     MedInstitution
                 </h3>
 
+                {/* Open / Close mobile menu */}
                 <div>
-                    <button className="md:hidden aspect-square h-full text-gray-400" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+                    <button className="aspect-square h-full text-gray-400 md:hidden" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
                         {isMobileMenuOpen ? 
                         (
-                        <div className='flex justify-center w-full h-full text-4xl'>
+                        <div className='flex h-full w-full justify-center text-4xl'>
                             &times;
                         </div>
                         )
                         : 
                         (
-                        <div className='flex justify-center items-center'>
+                        <div className='flex items-center justify-center'>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-9">
                                 <path fillRule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
                             </svg>
                         </div>
                         )}
                     </button>
-
-                    
                 </div>
-                <ul className="hidden md:flex items-center gap-5 px-5 font-bold text-gray-500 dark:text-gray-200">
+
+                {/* Desktop Navbar */}
+                <ul className="hidden items-center gap-5 px-5 font-bold text-gray-500 md:flex dark:text-gray-200">
                     <li className="hover:text-black dark:hover:text-blue-700"><a href="">Home</a></li>
                     <li className="hover:text-black dark:hover:text-blue-700"><a href="">Services</a></li>
                     <li className="text-blue-700"><button className='cursor-pointer' title='Current Page'>APPOINTMENTS</button></li>
                     <li className="hover:text-black dark:hover:text-blue-700">
                         <button onClick={toggleSettingsWindow} className='cursor-pointer flex items-center gap-1' title='Open Settings'>
-                        Settings
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
-                            <path fillRule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" clipRule="evenodd" />
-                        </svg>
-
+                            Settings
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+                                <path fillRule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" clipRule="evenodd" />
+                            </svg>
                         </button>
                     </li>
                 </ul>
