868294b1e80449d9f001bdeda69474cc74b66573
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-07-25 13:40:02 -0300

    fix/mobile: minor bottom styling toast

--- final_project/src/components/toast/Toast.jsx
+++ final_project/src/components/toast/Toast.jsx
@@ -7,7 +7,7 @@ function Toast({ message, type = "success", onClose }){
         info: "bg-orange-400 text-white dark:bg-amber-600 dark:text-gray-200"
     }
     return(
-        <div className={`fixed right-0 bottom-1/12 left-0 z-50 mx-5 rounded px-2 py-4 text-start shadow-md md:right-6 md:bottom-6 md:left-auto md:mx-5 md:text-start ${styles[type]}`}>
+        <div className={`fixed right-0 bottom-10 left-0 z-50 mx-5 rounded px-2 py-4 text-start shadow-md md:right-6 md:bottom-6 md:left-auto md:mx-5 md:text-start ${styles[type]}`}>
             <div className="flex items-center justify-between font-medium">
                 <span>{message}</span>
                 <button onClick={onClose} className='flex aspect-square h-full w-6 cursor-pointer items-center justify-center hover:text-gray-600'>
